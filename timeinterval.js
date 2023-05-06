c59b2a435b562bfe81734e2b5db1fee433b61b22
ruchita toke <tokeruchita@gmail.com>
2023-05-06 09:40:42 +0530

    initial commit

---
+++ timeinterval.js
@@ -0,0 +1,6 @@
+//It will print the message after every 3sec 
+
+let a=("HELLO WORLD")
+setInterval(function(){
+    console.log("I am inside")
+},3000)
---
+++ timeout.js
@@ -0,0 +1,6 @@
+//It will display the message after 3sec
+
+let a=("HELLO WORLD")
+setTimeout(function(){
+    console.log("I am inside")
+},3000)
